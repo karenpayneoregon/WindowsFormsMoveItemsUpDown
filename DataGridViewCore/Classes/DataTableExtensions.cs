08d811c5d8b67684cbbbb9afbe510fd56c4c9d47
Karen Payne <paynekaren@comcast.net>
2024-01-05 07:45:29 -0800

    overload added

--- DataGridViewCore/Classes/DataTableExtensions.cs
+++ DataGridViewCore/Classes/DataTableExtensions.cs
@@ -7,4 +7,8 @@ public static class DataTableExtensions
     public static List<RowItem> RowItems(this DataTable sender) =>
         sender.AsEnumerable()
             .Select((row, index) => new RowItem(index, row.Field<int>("ProductId"))).ToList();
+
+    public static List<RowItem> RowItems(this DataTable sender, string columnName) =>
+        sender.AsEnumerable()
+            .Select((row, index) => new RowItem(index, row.Field<int>(columnName))).ToList();
 }
